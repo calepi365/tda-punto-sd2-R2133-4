c630e0ad639a66f98041871fbddf7b4bbfdcc5a4
calepi365 <rcaleb090@gmail.com>
2024-08-15 18:02:21 -0400

    Presentacion de la segunda practica  de SD R2133-4 de los metodos dameDistancia y damePuntoMedio

--- Punto.java
+++ Punto.java
@@ -1,3 +1,4 @@
+import javax.annotation.processing.SupportedOptions;
 
 public class Punto implements IPunto {
 
@@ -95,20 +96,72 @@ public class Punto implements IPunto {
         this.etiqueta = etiqueta;
     }
 
-
     // implementar los metodos abstractos que permitan obtener la
     // 1. la distancia entre dos puntos
     // 2. el punto medio entre dos puntos
     // para presentar mediante git la proxima clase 12/08/2024
 
     @Override
-    public double dameDistanciaEntreOtroPunto(Punto OtroOPunto) {
+    public double dameDistanciaEntreOtroPunto(Punto OtroPunto) {
 
-        return 0;
+        double distancia = 0;
+        double x1 = this.x;
+        double y1 = this.y;
+        double x2 = OtroPunto.getX();
+        double y2 = OtroPunto.getY();
+        double z1 = this.z;
+        double z2 = OtroPunto.getZ();
+
+        System.out.println(this.toString());
+        System.out.println(OtroPunto.toString());
+
+        if (this.sistema == Sistema.PLANO && OtroPunto.getSistema() == Sistema.PLANO) {
+
+            distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+        } else if (this.sistema == Sistema.ESPACIO && OtroPunto.getSistema() == Sistema.ESPACIO) {
+            distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) + Math.pow((z2 - z1), 2));
+
+        } else {
+            System.out.println(" debe proporcionar un sistema , no se puede realizar la operacion. ");
+        }
+
+        return distancia;
     }
 
     @Override
     public Punto damePuntoMedioEntreOtroPunto(Punto OtroPunto) {
+     double mediodex = 0;
+     double mediodey = 0;
+     double mediodez = 0;
+
+     double puntodex1 = this.x;
+     double puntodey1 = this.y;
+     double puntodex2 = OtroPunto.getX();
+     double puntodey2 = OtroPunto.getY();
+     double puntodez1 = this.z;
+     double puntodez2 = OtroPunto.getZ();
+
+     System.out.println(this.toString());
+        System.out.println(OtroPunto.toString());
+
+     if (this.sistema == Sistema.PLANO  && OtroPunto.getSistema() == Sistema.PLANO){
+
+     mediodex = (puntodex1 + puntodex2)/2;
+     mediodey = (puntodey1 + puntodey2)/2;
+      
+     return new Punto(Sistema.PLANO,mediodex,mediodey);
+
+     }else if (this.sistema == Sistema.ESPACIO && OtroPunto.getSistema() == Sistema.ESPACIO) {
+        mediodex = (puntodex1 + puntodex2)/2;
+        mediodey = (puntodey1 + puntodey2)/2;
+        mediodez = (puntodez1 + puntodez2)/2;
+
+        return new Punto(Sistema.ESPACIO,mediodex,mediodey,mediodez);
+     }else{
+        System.out.println("debe proporcionar un sistema , no se puede realizar la operacion.");
+     }
+
 
         return null;
     }
